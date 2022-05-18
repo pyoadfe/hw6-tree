dbf34151c4e2fe8b019f062f3fd93a526d505149
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2022-05-18 10:48:44 +0300

    Add empty files

---
+++ galaxies.py
@@ -0,0 +1,4 @@
+from forest import RandomForestRegressor
+
+if __name__ == "__main__":
+	pass
---
+++ tree.py
@@ -0,0 +1,5 @@
+class BaseDecisionTree:
+	pass
+
+class DecisionTreeRegressor(BaseDecisionTree):
+	pass
